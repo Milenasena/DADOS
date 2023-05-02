35baadab8c4d36eba2db47f5bf40b740ec7358a3
Milenasena <milena.sen4@gmail.com>
2023-05-02 11:02:26 -0300

    DADOS

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.example.dados
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dados.ui.theme.DADOSTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +41,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RolarDado()
+
                 }
             }
         }
     }
 }
-
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun RolarDado(){
+    var numeroaleatorio by remember { mutableStateOf(1) }
+     Text(
+         text = "Play whit me",
+         fontFamily = FontFamily.Cursive,
+         textAlign = TextAlign.Center,
+         fontSize = 75.sp,
+         modifier = Modifier.fillMaxWidth()
+             .padding(top = 65.dp),
+         color = Color.Magenta,
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DADOSTheme {
-        Greeting("Android")
+
+     )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize ()
+
+
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.dice_1) ,
+            contentDescription = null
+        )
+
+        Button(
+            onClick = { numeroaleatorio = (1..6).random() }
+        )
+        {
+            Text(
+                text = "Rolar"
+
+            )
+
+        }
+
+
     }
+
+
 }
